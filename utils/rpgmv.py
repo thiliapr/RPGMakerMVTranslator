33d7f45742b50d0ac177e07706f6899bd6e7dbf6
thiliapr <140580293+thiliapr@users.noreply.github.com>
2024-07-10 14:32:02 +0800

    +name

--- utils/rpgmv.py
+++ utils/rpgmv.py
@@ -1,27 +1,44 @@
+from typing import Any
 from collections.abc import Callable
 from tprtools import jsonpath
 
 
 class RPGMakerMVData:
 	ItemTextsKeys: tuple[str] = ("name", "description", "note", "message1", "message2", "message3", "message4")
+	SystemJSONKeys: tuple[str] = ("currencyUnit", "gameTitle")
 
 	@staticmethod
-	def event(event: jsonpath.JSONObject, parent_path: str, **kwargs) -> list[str]:
+	def event(event: jsonpath.JSONObject, parent_path: str, **kwargs) -> list[dict[str, Any]]:
+		messages: list[dict[str, Any]] = []
+
 		# Choices
 		if event["code"] == 102:
-			return [jsonpath.concat_path(parent_path, f"$.parameters[0][{i}]") for i in range(len(event["parameters"][0])) if event["parameters"][0][i]]
+			paths = [f"$.parameters[0][{i}]" for i in range(len(event["parameters"][0])) if event["parameters"][0][i]]
+			return [{"message": jsonpath.get(event, path), "path": jsonpath.concat_path(parent_path, path)} for path in paths]
 		# Text
-		elif event["code"] == 401:
-			return [jsonpath.concat_path(parent_path, "$.parameters[0]")] if event["parameters"][0] else []
-		else:
-			return []
+		elif event["code"] == 401 and event["parameters"][0]:
+			message: dict[str, Any] = {
+				"message": event["parameters"][0],
+				"path": jsonpath.concat_path(parent_path, "$.parameters[0]")
+			}
+
+			# With Name
+			if message["message"].startswith("\\n<"):
+				message["name"] = message["message"][3:message["message"].find(">")]
+				message["message"] = message["message"][message["message"].find(">") + 1:]
+
+			# Add message info to messages
+			messages.append(message)
+
+		# Unknown Event
+		return messages
 
 	@staticmethod
-	def items(items: list, **kwargs) -> list[str]:
-		messages: list[str] = [
-			message_path
+	def items(items: list, **kwargs) -> list[dict[str, Any]]:
+		messages: list[dict[str, Any]] = [
+			{"message": jsonpath.get(items, path), "path": path}
 			for item_index in range(len(items))
-			for message_path in (
+			for path in (
 				[f"$[{item_index}].{key}" for key in RPGMakerMVData.ItemTextsKeys if items[item_index].get(key)]
 				if items[item_index] else []
 			)
@@ -30,28 +47,28 @@ class RPGMakerMVData:
 		return messages
 
 	@staticmethod
-	def troops(troops: list, **kwargs) -> list[str]:
-		messages: list[str] = RPGMakerMVData.items(troops)
+	def troops(troops: list, **kwargs) -> list[dict[str, Any]]:
+		messages: list[dict[str, Any]] = RPGMakerMVData.items(troops)
 
 		# Events
 		messages += [
-			message_path
+			message
 			for troop_index in range(len(troops))
 			for page_index in range(len(troops[troop_index]["pages"]) if troops[troop_index] else 0)
 			for event_index in range(len(troops[troop_index]["pages"][page_index]["list"]))
-			for message_path in RPGMakerMVData.event(troops[troop_index]["pages"][page_index]["list"][event_index], f"$[{troop_index}].pages[{page_index}].list[{event_index}]", **kwargs)
+			for message in RPGMakerMVData.event(troops[troop_index]["pages"][page_index]["list"][event_index], f"$[{troop_index}].pages[{page_index}].list[{event_index}]", **kwargs)
 		]
 
 		return messages
 
 	@staticmethod
-	def map_events(map_events: dict, **kwargs) -> list[str]:
-		messages: list[str] = [
-			message_path
+	def map_events(map_events: dict, **kwargs) -> list[dict[str, Any]]:
+		messages: list[dict[str, Any]] = [
+			message
 			for map_event_index in range(len(map_events["events"]))
 			for page_index in range(len(map_events["events"][map_event_index]["pages"]) if map_events["events"][map_event_index] else 0)
 			for event_index in range(len(map_events["events"][map_event_index]["pages"][page_index]["list"]))
-			for message_path in RPGMakerMVData.event(map_events["events"][map_event_index]["pages"][page_index]["list"][event_index], f"$.events[{map_event_index}].pages[{page_index}].list[{event_index}]", **kwargs)
+			for message in RPGMakerMVData.event(map_events["events"][map_event_index]["pages"][page_index]["list"][event_index], f"$.events[{map_event_index}].pages[{page_index}].list[{event_index}]", **kwargs)
 		]
 
 		# Display Name
@@ -61,37 +78,33 @@ class RPGMakerMVData:
 		return messages
 
 	@staticmethod
-	def common_events(events: list, **kwargs) -> list[str]:
-		messages: list[str] = [
-			message_path
+	def common_events(events: list, **kwargs) -> list[dict[str, Any]]:
+		messages: list[dict[str, Any]] = [
+			message
 			for common_event_index in range(len(events))
 			for event_index in range(len(events[common_event_index]["list"]) if events[common_event_index] else 0)
-			for message_path in RPGMakerMVData.event(events[common_event_index]["list"][event_index], f"$[{common_event_index}].list[{event_index}]")
+			for message in RPGMakerMVData.event(events[common_event_index]["list"][event_index], f"$[{common_event_index}].list[{event_index}]")
 		]
 
 		return messages
 
 	@staticmethod
-	def system_json(system_json: dict, **kwargs) -> list[str]:
-		messages: list[str] = []
-
-		# Outside
-		messages.append("$.currencyUnit")
-		messages.append("$.gameTitle")
+	def system_json(system_json: dict, **kwargs) -> list[dict[str, Any]]:
+		paths: list[dict[str, Any]] = [k for k in RPGMakerMVData.SystemJSONKeys]
 
 		# Terms
-		messages += [
+		paths += [
 			f"$.terms.{term_key}.{('[%s]' if isinstance(system_json['terms'][term_key], list) else '%s') % item_key}"
 			for term_key in system_json["terms"]
 			for item_key in (system_json["terms"][term_key] if isinstance(system_json["terms"][term_key], dict) else range(len(system_json["terms"][term_key])))
 			if system_json["terms"][term_key][item_key]
 		]
 
-		return messages
+		return [{"message": jsonpath.get(system_json, path), "path": path} for path in paths]
 
 	@staticmethod
-	def extract_data(filename: str, data: jsonpath.JSONObject, **kwargs) -> list[str]:
-		extract_func: Callable[[jsonpath.JSONObject, ...], list[str]]
+	def extract_data(filename: str, data: jsonpath.JSONObject, **kwargs) -> list[dict[str, Any]]:
+		extract_func: Callable[[jsonpath.JSONObject, ...], list[dict[str, Any]]]
 
 		# Setting Extract Function
 		if filename == "CommonEvents.json":
